9181e5f0125e8884190b32925a01266628b6f7a8
Scott Young <young.scottw@gmail.com>
2017-04-17 13:18:02 -0500

    Done.

--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
 import { createStore } from 'redux'
-import rootReducer from './reducers/index'
+import combineReducers from './reducers/index'
 
 export function configureStore(){
-  return createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  return createStore(combineReducers, /*window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()*/)
 }
 
 export const store = configureStore()
